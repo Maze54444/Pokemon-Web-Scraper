bf18ba6fe493941d5a5ded665efe4fec6cce450b
Maze54444 <chickfrizzle89@gmail.com>
2025-04-07 23:44:14 +0200

    fix: aktualisierter Bot-Token & Chat-ID

--- scraper.py
+++ scraper.py
@@ -93,7 +93,7 @@ def run_once(seen):
 
 if __name__ == "__main__":
     print("📦 Hauptblock wurde erreicht", flush=True)
-    seen = set()
+    seen = load_seen()
     while True:
         interval = run_once(seen)
         time.sleep(interval)
